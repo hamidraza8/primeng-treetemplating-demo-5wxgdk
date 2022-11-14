5c7f88fcf49adbb04c8354d5e216381f5847490a
hamidraza8 <48580151+hamidraza8@users.noreply.github.com>
2022-11-14 15:17:42 +0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,39 @@ import { NodeService } from './nodeservice';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  grid: number[][];
+  grid: any[][];
   grid_cols_n: number;
   grid_rows_n: number;
-
+  isMouseDown = false;
   setGridSize(cols_n, rows_n) {
     this.grid_cols_n = cols_n;
     this.grid_rows_n = rows_n;
   }
-  constructor() {}
+  constructor() {
+    this.bindGrid();
+  }
 
   ngOnInit() {}
-
+  setMouseDown(mouseDown, id) {
+    this.fillBlock(id);
+    this.isMouseDown = mouseDown;
+  }
+  fillBlock(id) {
+    if (this.isMouseDown) {
+      var element = document.getElementById(id);
+      if (element != undefined) {
+        console.log(id);
+        element.style.backgroundColor = 'red';
+      }
+    }
+  }
   bindGrid() {
-    for (var y = 0; y < this.grid_cols_n; y++) {
+    this.setGridSize(30, 30);
+    this.grid = [[]];
+    for (var y = 0; y < this.grid_rows_n; y++) {
       this.grid.push([]);
-      for (var x = 0; x < this.grid_rows_n; x++) {
-        this.bindGrid[y].push([y, x]);
+      for (var x = 0; x < this.grid_cols_n; x++) {
+        this.grid[y].push([y, x]);
       }
     }
   }
