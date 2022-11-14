8b8e233c36a6bea3c8bbc36c30de4fc1944d32ce
hamidraza8 <48580151+hamidraza8@users.noreply.github.com>
2022-11-14 14:18:09 +0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,22 @@
-import {Component,OnInit} from '@angular/core';
-import {NodeService} from './nodeservice';
-import {TreeNode} from 'primeng/api';
-
+import { Component, OnInit } from '@angular/core';
+import { NodeService } from './nodeservice';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent { 
-     nodes: TreeNode[];
-    
-    constructor() { }
+export class AppComponent {
+  grid: number[][];
+  grid_cols_n: number;
+  grid_rows_n: number;
 
-    ngOnInit() {
-        this.nodes = [
-            {
-                key: '0',
-                label: 'Introduction',
-                children: [
-                    {key: '0-0', label: 'What is Angular', data:'https://angular.io', type: 'url'},
-                    {key: '0-1', label: 'Getting Started', data: 'https://angular.io/guide/setup-local', type: 'url'},
-                    {key: '0-2', label: 'Learn and Explore', data:'https://angular.io/guide/architecture', type: 'url'},
-                    {key: '0-3', label: 'Take a Look', data: 'https://angular.io/start', type: 'url'}
-                ]
-            },
-            {
-                key: '1',
-                label: 'Components In-Depth',
-                children: [
-                    {key: '1-0', label: 'Component Registration', data: 'https://angular.io/guide/component-interaction', type: 'url'},
-                    {key: '1-1', label: 'User Input', data: 'https://angular.io/guide/user-input', type: 'url'},
-                    {key: '1-2', label: 'Hooks', data: 'https://angular.io/guide/lifecycle-hooks', type: 'url'},
-                    {key: '1-3', label: 'Attribute Directives', data: 'https://angular.io/guide/attribute-directives', type: 'url'}
-                ]
-            }
-        ];
-    }
+  setGridSize(cols_n, rows_n) {
+    this.grid_cols_n = cols_n;
+    this.grid_rows_n = rows_n;
+  }
+  constructor() {}
+
+  ngOnInit() {}
+
+  bindGrid() {}
 }
